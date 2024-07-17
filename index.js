910e50e46722a3094331615784dc9cd5d8b128ee
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-07-17 10:40:56 +0600

    post userInfo to ta databse

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express();
+const bcrypt = require('bcryptjs');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -12,6 +14,8 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6u1ikeh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -23,8 +27,34 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
+    const usersCollection =client.db('mfs').collection('users')
 
+    app.post('/register', async (req, res) => {
+      const { name, pin, phone, email, role } = req.body;
+      
+      if (!/^\d{5}$/.test(pin)) {
+        return res.status(400).send({ error: 'PIN must be a 5-digit number' });
+      }
+  
+      const hashedPin = await bcrypt.hash(pin, 10);
+      const newUser = {
+        name,
+        pin: hashedPin,
+        phone,
+        email,
+        role,
+        status: 'pending',
+        balance: 0,
+      };
+  
+      try {
+        const result = await usersCollection.insertOne(newUser);
+        res.status(201).send({ message: 'User registered successfully', userId: result.insertedId });
+      } catch (error) {
+        res.status(500).send({ error: 'Failed to register user' });
+      }
+    });
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -34,7 +64,7 @@ async function run() {
     // await client.close();
   }
 }
-// run().catch(console.dir);
+run().catch(console.dir);
 
 
 
